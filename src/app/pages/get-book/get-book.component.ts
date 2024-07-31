300c300ed25a4d64a4080cc9218eab28730edeaa
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-07-30 21:29:07 -0300

    creating get-book component

---
+++ src/app/pages/get-book/get-book.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-book',
+  standalone: true,
+  imports: [],
+  templateUrl: './get-book.component.html',
+  styleUrl: './get-book.component.css'
+})
+export class GetBookComponent {
+
+}
