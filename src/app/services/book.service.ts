7899f93b7ceb42f71181d0fa50c5fb10cdde9378
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-07-31 20:10:10 -0300

    creating getAllBooks method

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,2 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Book } from '../models/Book';
+
+@Injectable({
+  providedIn: 'root'
+})
 export class BookService {
+  private apiUrl = 'http://localhost:8080/books'
+
+  constructor(private http : HttpClient) { }
+
+  getAllBooks(){
+    return this.http.get<Book[]>(this.apiUrl).toPromise();
+  }
 }
