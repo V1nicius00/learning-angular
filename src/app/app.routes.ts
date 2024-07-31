8b174d1975d25512afb4c18c62318ea23a137051
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-07-30 21:32:43 -0300

    creating routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,29 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { CreateBookComponent } from './pages/create-book/create-book.component';
+import { UpdateBookComponent } from './pages/update-book/update-book.component';
+import { GetBookComponent } from './pages/get-book/get-book.component';
+import { DeleteBookComponent } from './pages/delete-book/delete-book.component';
 
 export const routes: Routes = [
     {
         path: "",
-        component: HomeComponent
+        component: HomepageComponent
+    },
+    {
+        path: "create",
+        component: CreateBookComponent
+    },
+    {
+        path: "update",
+        component: UpdateBookComponent
+    },
+    {
+        path: "get",
+        component: GetBookComponent
+    },
+    {
+        path: "delete",
+        component: DeleteBookComponent
     }
 ];
