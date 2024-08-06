e393a19a794f9c94ccb39d5f5674a5ab8effd8ce
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-08-05 21:29:48 -0300

    creating createBook component

--- src/app/components/create-book/create-book.component.ts
+++ src/app/components/create-book/create-book.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
+import { BookService } from '../../services/book.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-book',
   standalone: true,
-  imports: [NavComponent],
+  imports: [
+    NavComponent,
+    FormsModule
+  ],
   templateUrl: './create-book.component.html',
   styleUrl: './create-book.component.css'
 })
 export class CreateBookComponent {
 
+  constructor(private bookService : BookService){};
+
+  bookName: string = '';
+  author: string = '';
+  description: string = '';
+  price: number = 0;
+  releaseDate: Date = new Date();
+
+  createBook(): void{
+    const book = {
+      bookName: this.bookName,
+      author: this.author,
+      description: this.description,
+      price: this.price,
+      releaseDate: this.releaseDate
+    }
+
+    this.bookService.createBook(book)
+    .then(book => console.log(book))
+    .catch(error => console.error(error))
+  }
 }
