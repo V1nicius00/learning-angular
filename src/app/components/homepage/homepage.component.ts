192887cd2266461b238b30630bb8ebfce5d69450
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-08-09 23:43:09 -0300

    updating homepage

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
+import { CommonModule } from '@angular/common';
+import { Book } from '../../models/Book';
+import { BookService } from '../../services/book.service';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [NavComponent],
+  imports: [
+    NavComponent,
+    CommonModule
+  ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
 
+  data: Book[] = [];
+
+  constructor(private bookService : BookService){}
+
+  ngOnInit(): void{
+    this.bookService.getAllBooks()
+    .then(books => {
+      if(books){
+        this.data = books
+      } else{
+        this.data = []
+      }
+      console.log(this.data)
+    })
+    .catch(error => console.error(error));
+  }
+
+  deleteBook(bookId: string): void {
+    this.bookService.deleteBook(bookId)
+    .then(book => {
+      console.log(book)
+      this.ngOnInit()
+    })
+    .catch(error => console.error(error))
+  }
 }
